1787ee9c970d625a835dc6d47183f581f68fb403
DreadPirateRoberts <46958197+ManfredRichthofen@users.noreply.github.com>
2023-03-28 12:32:08 -0700

    Add stuffs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,26 +4,51 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   outDir: '../dist',
   title: "Movies",
-  description: "Media Server Guides",
+  description: "Server Guides",
+  
+  
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Guides', link: '/apps' }
+      { text: 'Guides', link: '/jellyfin/apps' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: 'Jellyfin Guides',
+        collapsed: false,
+        items: [
+          { text: 'Apps', link: '/jellyfin/apps' },
+          { text: 'Requesting Movies', link: '/jellyfin/requests' },
+          { text: 'Request an Account', link: '/jellyfin/account' },
+          { text: 'Server Status', link: '/jellyfin/status' }
+        ]
+      },
+      {
+        text: 'Server Hosting Guides',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: 'Upload Server Files', link: '/api-examples' }
+        ]
+      },
+      {
+        text: 'Discord Bot Guides',
+        items: [
+          { text: 'Music Bot', link: '/discord-bots/markdown-examples' },
         ]
       }
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'twitter', link: '...' },
+      {
+        icon: {
+          svg: '<img src = "/imgs/jellyfin.svg" alt="jellyfin" width="24" height="24"/>'
+        },
+        link: 'https://jfapp.xyz'
+      }
     ]
   }
 })
---
+++ docs/jellyfin/apps.md
@@ -0,0 +1,30 @@
+# Web Browser
+Visit the [Jellyfin Website](https://jfapp.xyz)
+### Common Issues
+
+#### Videos Wont Play
+
+### Downloads
+
+# Android TV/Fire TV
+
+### Common Issues
+
+### Downloads
+You can search for Jellyfin in the appstore of your device or visit the store pages
+
+[Android TV Store Page](https://play.google.com/store/apps/details?id=dev.jdtech.jellyfin&hl=en_US&gl=US)
+
+[Fire TV Store Page]
+
+# Ios/Apple TV
+
+### Common Issues
+
+### Downloads
+
+# Windows
+
+### Common Issues
+
+### Downloads
